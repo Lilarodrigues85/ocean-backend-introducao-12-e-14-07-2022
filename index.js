f4b931e314f84c0a07a9e76a6c5b63878b9724c6
Lila Rodrigues <107584756+Lilarodrigues85@users.noreply.github.com>
2022-07-12 12:21:47 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+  res.send('Hello World');
+});
+// /oi -> "Olá, Mundo"
+app.get('/oi', function (req, res) {
+    res.send('Olá, Mundo!');
+  });
 
-app.listen(3000)
+  //endpoints de heróis
+
+  const herois = ["Mulher Maravilha", "Capitã Marvel", "Homem de Ferro"];
+  //                0                   1               2
+
+  //[GET] /heois
+  app.get("/herois", function (req, res) {
+      res.send(herois);
+  });
+
+  app.listen(3000, function () {
+    console.log("Aplicação rodando em http://localhost:3000");
+});
